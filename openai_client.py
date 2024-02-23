c1e8c09f74784dc387872174c1725c87690d82a0
neekesh <nikesthapa20@gmail.com>
2024-02-23 11:47:54 +0545

    feat: wait for existing thread

--- openai_client.py
+++ openai_client.py
@@ -2,53 +2,47 @@ import openai
 import time
 import os
 from dotenv import load_dotenv
+from datastore import Datastore
 
-PROMPT = "Detailed description of how chat gpt builder will work just as detailed as you want "
 load_dotenv()
+datastore = Datastore()
 class OpenAIClient:
+    
+  
 
-    def complete(self, question):
+    def complete(self, question, phone_no):
         client = openai.OpenAI(
             api_key=os.getenv("OPENAI_API_KEY")
         )
 
-        assistant = client.beta.assistants.retrieve(os.getenv("OPENAI_ASSISTANT_ID"))
-        thread = client.beta.threads.create()
-
+        assistant = client.beta.assistants.retrieve(
+            os.getenv("OPENAI_API_KEY"),
+            )
+        thread_id = datastore.get(phone_no)
+        if thread_id is None:
+            thread = client.beta.threads.create()
+            thread_id = thread.id
+            datastore.create(chat_details={
+                "phone_no": phone_no,
+                "thread_id": thread.id
+            })
         client.beta.threads.messages.create(
-            thread_id=thread.id, role="user", content=question
+            thread_id=thread_id, role="user", content=question
         )
 
-        client.beta.threads.runs.create(
-            thread_id=thread.id, assistant_id=assistant.id
+        run = client.beta.threads.runs.create(
+            thread_id=thread_id, assistant_id=assistant.id
         )
-
-        # completed_run = wait_on_run(run, thread)
-
-        time.sleep(30) # INSERTING DELAY HERE HELPED
-
-        messages = client.beta.threads.messages.list(thread_id=thread.id, order="desc")
-        new_message = messages.data[0].content[0].text.value
-    
-#         response = client.chat.completions.create(
-# model="gpt-3.5-turbo",
-#         #   prompt=PROMPT,
-#         #   temperature=0.0,
-#         #   max_tokens=256,
-#         #   top_p=1,
-#           messages = [
-#               {"role": "user", "content": question}
-#           ],
-#         #   frequency_penalty=0,
-#         #   presence_penalty=0,
-#         )
-
-        # print ("response form openai is :\n" + str(response.choices[0].message.content) + "\n")
-        return new_message
+        
+        while True:
+            run = client.beta.threads.runs.retrieve(
+                thread_id=thread_id,
+                run_id=run.id
+                )
+            if run.completed_at:
+                messages = client.beta.threads.messages.list(thread_id=thread_id, order="desc")
+                return messages.data[0].content[0].text.value
 
 if __name__ == "__main__":
     client = OpenAIClient()
     load_dotenv()
-    response = client.complete("how are you")
-    print (response)
-    
